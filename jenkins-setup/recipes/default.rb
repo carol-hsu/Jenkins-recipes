81cc4ef1034fc1c9ab7fe0b64c95c3f14938d5d6
carol-hsu <asymplone@gmail.com>
2015-06-01 16:13:47 +0800

    master installation

--- jenkins-setup/recipes/default.rb
+++ jenkins-setup/recipes/default.rb
@@ -1,6 +1,13 @@
-remote_file "/etc/yum.repos.d/jenkins.repo" do
-	source "http://pkg.jenkins-ci.org/redhat/jenkins.repo"
+remote_file '/etc/yum.repos.d/jenkins.repo' do
+	source 'http://pkg.jenkins-ci.org/redhat/jenkins.repo'
 end
 
+execute 'rpm-import' do 
+	command 'rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key'
+	not_if 'rpm --help'
+end
 
+yum_package ['jenkins','java'] do
+	action :install
+end
 
