e9d702b25b40fa113ec7c3533066af98482ccbeb
carol-hsu <asymplone@gmail.com>
2015-06-18 11:10:24 +0800

    grant user permission

--- jenkins-slave-setup/recipes/default.rb
+++ jenkins-slave-setup/recipes/default.rb
@@ -1,7 +1,32 @@
-directory '/home/ec2-user/jenkins' do
-    owner 'ec2-user'
-    group 'ec2-user'
+user 'jenkins' do
+    home '/home/jenkins'
+    shell '/bin/bash'
+    password '$1$RDXR2jjd$vhiK.lztBQ43bjtr0xIjM/'
+    action :create
 end
+
+directory '/home/jenkins' do
+    owner 'jenkins'
+    group 'jenkins'
+end
+
+directory '/home/jenkins/.ssh' do
+    owner 'jenkins'
+    group 'jenkins'
+	mode '0700'
+end
+
+#grant user permission to jenkins
+bash 'add-public-key' do
+	user 'root'
+	code <<-EOH
+	cat /home/ec2-user/.ssh/authorized_keys >> /home/jenkins/.ssh/authorized_keys
+	chmod 600 /home/jenkins/.ssh/authorized_keys
+	chown jenkins /home/jenkins/.ssh/authorized_keys
+	chgrp jenkins /home/jenkins/.ssh/authorized_keys
+	EOH
+end
+
 
 bash 'install-ant' do
 	user 'root'
