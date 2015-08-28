50c7ca8a59b293f04be22e59529cc245f4f5521e
nosus <carol@tw-carolhsu-2.client.tw.trendnet.org>
2015-08-28 18:26:15 +0800

    docker version downgrade

--- jenkins-slave-setup/recipes/default.rb
+++ jenkins-slave-setup/recipes/default.rb
@@ -41,8 +41,7 @@ end
 bash 'install-docker' do
 	user 'root'
 	code <<-EOH
-	curl -O -sSL https://get.docker.com/rpm/1.7.0/centos-6/RPMS/x86_64/docker-engine-1.7.0-1.el6.x86_64.rpm
-	yum -y localinstall --nogpgcheck docker-engine-1.7.0-1.el6.x86_64.rpm
+	yum -y install docker
 	service docker start
 	usermod -aG docker jenkins
 	echo "# /etc/sysconfig/docker" > /etc/sysconfig/docker
