5a24758001d8324bde882a45eb4f6ee822b73b01
nosus <carol_hsu@trend.com.tw>
2015-09-03 11:35:45 +0800

    test

--- jenkins-slave-setup/recipes/create-workdir.rb
+++
@@ -1,9 +0,0 @@
-#create dir
-directory '/media/ephemeral0/jenkins' do
-    owner jenkins
-    group jenkins
-	mode '0755'
-	action :create
-    recursive true
-end
-
--- jenkins-slave-setup/recipes/default.rb
+++ jenkins-slave-setup/recipes/default.rb
@@ -49,4 +49,13 @@ bash 'install-docker' do
 	EOH
 end
 
+#create dir
+directory '/media/ephemeral0/jenkins' do
+    owner 'jenkins'
+    group 'jenkins'
+    mode '0755'
+    action :create
+    recursive true
+end
+
 Chef::Log.info("***************** Jenkins slave setup finished **************")
