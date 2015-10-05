26f7bbc5f84bdd7db49a22f2ba07378480935ecc
nosus <carol_hsu@trend.com.tw>
2015-10-05 15:34:31 +0800

    add jenkins public key

---
+++ jenkins-slave-setup/recipes/add-jenkins-admin.rb
@@ -0,0 +1,28 @@
+#add jenkins public key in user "jenkins"'s authorized file
+file '/home/jenkins/.ssh/authorized_keys' do
+    owner 'jenkins'
+    group 'jenkins'
+    mode '0600'
+    content "#{node[:jenkins_pubkey]}"
+    action :create
+end
+
+#grant user permission to jenkins
+bash 'add-public-key' do
+    user 'root'
+    code <<-EOH
+    cat /home/ec2-user/.ssh/authorized_keys >> /home/jenkins/.ssh/authorized_keys
+    echo "jenkins  ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoer
+    EOH
+end
+
+#create dir
+directory '/media/ephemeral0/jenkins' do
+    owner 'jenkins'
+    group 'jenkins'
+    mode '0755'
+    action :create
+    recursive true
+end
+
+Chef::Log.info("***************** Jenkins slave configure finished **************")
--- jenkins-slave-setup/recipes/default.rb
+++ jenkins-slave-setup/recipes/default.rb
@@ -15,24 +15,6 @@ directory '/home/jenkins/.ssh' do
 	mode '0700'
 end
 
-#add jenkins public key in user "jenkins"'s authorized file
-file '/home/jenkins/.ssh/authorized_keys' do
-	owner 'jenkins'
-    group 'jenkins'
-    mode '0600'
-    content "#{node[:jenkins_pubkey]}"
-    action :create
-end
-
-#grant user permission to jenkins
-bash 'add-public-key' do
-	user 'root'
-	code <<-EOH
-	cat /home/ec2-user/.ssh/authorized_keys >> /home/jenkins/.ssh/authorized_keys
-	echo "jenkins  ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoer
-	EOH
-end
-
 bash 'install-ant' do
 	user 'root'
 	code <<-EOH
@@ -53,15 +35,6 @@ bash 'install-docker' do
 	echo 'other_args="--insecure-registry dcsrd-docker-registry.trendmicro.com"' >> /etc/sysconfig/docker
 	service docker restart
 	EOH
-end
-
-#create dir
-directory '/media/ephemeral0/jenkins' do
-    owner 'jenkins'
-    group 'jenkins'
-    mode '0755'
-    action :create
-    recursive true
 end
 
 Chef::Log.info("***************** Jenkins slave setup finished **************")
